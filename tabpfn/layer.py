a8146900fa39040acc100036e9986101765f4e29
Antanas Murelis <120945116+AntanasMurelis@users.noreply.github.com>
2022-12-23 13:21:29 +0000

    Decoder comments
    Comments for the two decoders: Scaled Decoder - a single layer perceptron that is activated with GELU. One thing left unclear: the normalization step - uses softmax @ vector to normalize output layer. The second fixed scaled decoder functions the same but the scaling at the end is done by a 10 thousand vector which is summed - perhaps this is used for the Bayesian inference that is talked about in the paper

--- tabpfn/layer.py
+++ tabpfn/layer.py
@@ -120,8 +120,8 @@ class TransformerEncoderLayer(Module):
             else: # so we actually do this part
                 src2 = self.self_attn(src_, src_, src_, attn_mask=src_mask,
                                       key_padding_mask=src_key_padding_mask)[0]
-        src = src + self.dropout1(src2)
-        if not self.pre_norm: # this gets RUN: pre_norm=False so not False is True
+        src = src + self.dropout1(src2) 
+        if not self.pre_norm: # this gets RUN: pre_norm=False, not False = True
             src = self.norm1(src)
 
         if self.pre_norm: # NOT RUN: pre_norm=False
@@ -131,6 +131,6 @@ class TransformerEncoderLayer(Module):
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src_))))
         src = src + self.dropout2(src2)
 
-        if not self.pre_norm: # this gets RUN: pre_norm=False so not False is True
+        if not self.pre_norm: # this gets RUN: pre_norm=False, not False = True
             src = self.norm2(src)
         return src
