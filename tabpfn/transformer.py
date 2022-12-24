fc39e37f06ce3f8d630e903b1b31ecd77b5fc491
Antanas Murelis <120945116+AntanasMurelis@users.noreply.github.com>
2022-12-24 12:27:33 +0000

    Transformer Comments

--- tabpfn/transformer.py
+++ tabpfn/transformer.py
@@ -56,7 +56,7 @@ class TransformerModel(nn.Module):
         """Generates an upper triangular matrix with -inf and 0.0
 
         Args:
-            sz (int): size
+            sz (int): Batch size
 
         Returns:
             tensor: mask - upper triangular matrix
@@ -71,8 +71,8 @@ class TransformerModel(nn.Module):
         and 0 entries are -inf
 
         Args:
-            sz (int): size of the input - x and y
-            query_size (int): size of x
+            sz (int): batch size
+            query_size (int): number of query
 
         Returns:
             tensor: mask that masks y but attends itself (diagonal 0.0 NOT -inf)
@@ -105,7 +105,7 @@ class TransformerModel(nn.Module):
         
     @staticmethod
     def generate_global_att_trainset_matrix(num_global_att_tokens, seq_len, num_query_tokens):
-        """Directs attention between the trainset: essentially fully connected
+        r"""Directs attention between the trainset: essentially fully connected
 
         Args:
             num_global_att_tokens (int): 
@@ -113,7 +113,7 @@ class TransformerModel(nn.Module):
             num_query_tokens (int): 
 
         Returns:
-            _type_: trainset_size x num_global_tokens
+            tensor: (seq_len + num_global_att_tokens - num_query_tokens) x num_global_tokens
         """
         train_size = seq_len + num_global_att_tokens - num_query_tokens
         trainset_size = seq_len - num_query_tokens
