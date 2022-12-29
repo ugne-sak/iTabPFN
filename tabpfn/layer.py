b315a9a12e8760e650ed4e0a3bd422a82b98e97d
Antanas Murelis <120945116+AntanasMurelis@users.noreply.github.com>
2022-12-29 19:17:21 +0000

    No changes

--- tabpfn/layer.py
+++ tabpfn/layer.py
@@ -76,6 +76,8 @@ class TransformerEncoderLayer(Module):
         Shape:
             see the docs in Transformer class.
         """
+        
+        print("Forward")
         if self.pre_norm: # NOT RUN: pre_norm=False by default and is not changed in model=TransformerModel() in train.py
             src_ = self.norm1(src)
         else: # this gets RUN
