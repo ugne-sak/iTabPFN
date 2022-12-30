1aa8deca38bced60a4c414ef2729c7c940790b9a
this-is-ugne <ugapapugaa@gmail.com>
2022-12-30 12:56:22 +0200

    minor changes

--- tabpfn/layer.py
+++ tabpfn/layer.py
@@ -139,7 +139,7 @@ class TransformerEncoderLayer(Module):
             src_left_ = src_[:single_eval_position]
             src_right_ = src_[single_eval_position:] # <- split the data
             
-            src_left_ = rearrange(src_left_, 'b h w -> w (b h') #
+            src_left_ = rearrange(src_left_, 'b h w -> w (b h)') #
             src_right_ = rearrange(src_right_, 'b h w -> w (b h) 1') # <- rearrange for Interfeature attention
 
             src_left_ = self.pre_linear1(src_left_) # 
